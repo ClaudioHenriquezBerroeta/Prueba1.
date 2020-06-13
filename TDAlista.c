0de89254cd293aba68ff852cc7c582be05286d4c
Jorge Oehrens <44478446+JorgeOehrens@users.noreply.github.com>
2020-06-13 15:52:46 -0400

    Update TDAlista.c

--- TDAlista.c
+++ TDAlista.c
@@ -1,14 +1,14 @@
 
-void promedio(struct lista ){
-	int contador =0;
-	int suma = 0;
+void promedio(struct lista ){    /* Se creo el nombre de la funcion*/
+	int contador =0;    /* Contador que nos servira para la divisio */
+	int suma = 0;       /* Variable que guardara la suma total de los numeros*/
 	float promedio=0;
-		for (x=0;x<lista->ultimo;x++){
+		for (x=0;x<lista->ultimo;x++){   /* Se recorrera desde 0 hasta el ultimo valor de la lista */
 			
-			suma = suma + lista->arreglo[x];
-			contador= contador+1;
+			suma = suma + lista->arreglo[x];     /* se iran sumando los numeros de la posicion x , con la  acumulacion de la variable suma*/
+			contador= contador+1;  /* Se calculara cuantos numero existen en la lista*/
 
 	}
-	promedio= suma/contador; 
-	return promedio;
+	promedio= suma/contador;    /* Calculo del promedio */
+	return promedio;    /*  Nuestra funcion finalmente retornara el promedio */ 
 }
